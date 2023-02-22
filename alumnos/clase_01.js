698a32f8d447cd3a02b23b2d114034eb68f1f699
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-02-22 13:31:39 -0300

    Create clase_01.js

---
+++ alumnos/clase_01.js
@@ -0,0 +1,9 @@
+const CentimetrosPorPulgadas = 2.54;
+
+let pulgadas = 50;
+let centimetros = pulgadas * CentimetrosPorPulgadas;
+
+let mensaje = `- ${pulgadas} in. es igual a ${centimetros} cm.`;
+
+console.log('Ejemplo de un programa para convertir pulgadas a centímetros v.2')
+console.log(mensaje);
