cf48334e0dc0fab7623f2ec91ab77e7a253712ed
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-02-21 23:49:47 -0300

    Agregamos la posibilidad de crear carpetas individuales a cada alumno

--- alumnos/separar.rb
+++ alumnos/separar.rb
@@ -1,21 +1,15 @@
 require "csv"
 require 'fileutils'
 
-def crear_carpetas(datos)
+def crear_carpetas(datos, folder = nil)
     puts "Generando carpetas para #{datos.count} alumnos"
     datos.each do |f|
-        FileUtils.mkdir_p "#{f[:notes]}/#{f[:folder]}"
+        FileUtils.mkdir_p  [f[:notes], f[:folder], folder].compact.join("/")
     end
 end
 
 alumnos = CSV.read("alumnos.csv", headers: true, header_converters: :symbol, encoding: "windows-1252:UTF-8", col_sep: ";")
-p alumnos.headers
+# p alumnos.headers
 crear_carpetas alumnos
 
-# csv.sort_by{|x|x[:nombre]}.uniq{|x|x[:nombre]}.each do |f|
-#     n = f[:apellidos].to_i 
-#     nombre, apellido = separar(f[:nombre], f[:apellidos].to_i)
-#     pp [n, nombre, apellido, f[:email], f[:curso], f[:nombre], ]
-#     salida << [apellido, nombre, f[:email], f[:curso]]
-# end
 
